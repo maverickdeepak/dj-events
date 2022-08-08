60dd6b301321a6622275616df22abeacfcf41e3c
Deepak <deepakkumarsataun60@gmail.com>
2022-08-08 16:25:32 +0530

    Initial Commit

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <div>About Page</div>
+      <p>This an app to find the latest DJ and other musical events.</p>
+      <p>Version 1.0.0</p>
+    </>
+  );
+};
+
+export default About;
